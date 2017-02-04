81e6aaa81d50299919d1c3ced6bf835e14800be9
Pal Lockheart <ex@palx.org>
2017-02-04 17:36:08 +0800

    add verbose zip log

--- unziplzfse/main.m
+++ unziplzfse/main.m
@@ -11,8 +11,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        [SSZipArchive unzipFileAtPath:[NSString stringWithUTF8String:argv[1]] toDestination:[NSString stringWithUTF8String:argv[2]]];
+        if( argc < 2 ) {
+            NSLog(@"Please provide a zip!");
+            return -1;
+        }
+        NSString *srcPath = [NSString stringWithUTF8String:argv[1]];
+        NSString *dstPath = argc > 2 ? [NSString stringWithUTF8String:argv[2]] : @".";
+        [SSZipArchive unzipFileAtPath:srcPath
+                        toDestination:dstPath
+                      progressHandler:^(NSString * _Nonnull entry, unz_file_info zipInfo, long entryNumber, long total) {
+                          NSLog(@"%ld/%ld unzipping file %s",entryNumber,total,[entry UTF8String]);
+                      }
+                    completionHandler:^(NSString * _Nonnull path, BOOL succeeded, NSError * _Nonnull error){
+                          NSLog(@"finished");
+                      }];
     }
     return 0;
 }
